6f25926a31c7e48ff855b2419673323bc9605d2a
hanchangmeo <hanchiangzo@gmail.com>
2025-04-16 01:40:44 +0700

    Update sigma_to_ndjson.sh

--- ci-scripts/sigma_to_ndjson.sh
+++ ci-scripts/sigma_to_ndjson.sh
@@ -15,6 +15,8 @@ for rule in ./rules/*.yml; do
     
     # Gọi lệnh sigma convert cho file rule này, tạo file NDJSON riêng
     sigma convert -t lucene -p "$PIPELINE_PATH" -o kibana/"${filename}.ndjson" "$rule"
+        # Sau đó, dùng jq để gộp nội dung JSON thành một dòng duy nhất
+    jq -c . kibana/"${filename}.ndjson" > kibana/"${filename}.tmp" && mv kibana/"${filename}.tmp" kibana/"${filename}.ndjson"
 done
 
 echo "Chuyển đổi hoàn tất. Các file NDJSON được lưu trong thư mục kibana."
